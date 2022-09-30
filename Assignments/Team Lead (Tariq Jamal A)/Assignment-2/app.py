a08d6041cf2779381d7cd0aa5a8356e1f07fbf26
Tariq Jamal A <tariqjamal4267324@gmail.com>
2022-09-30 11:59:52 +0530

    Connected flask app with ibm_db2

---
+++ Assignments/Team Lead (Tariq Jamal A)/Assignment-2/app.py
@@ -0,0 +1,65 @@
+from turtle import st
+from flask import Flask, render_template, request, redirect, url_for, session
+from markupsafe import escape
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+import ibm_db
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=ea286ace-86c7-4d5b-8580-3fbfa46b1c66.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31505;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=qhv23101;PWD=wV4fVnWHguaZJIHY",'','')
+
+# conn = ibm_db.connect("DATABASE={os.getenv(DATABASE_NAME)};HOSTNAME={os.getenv(HOST_NAME)};PORT={os.getenv(PORT_NUMBER)};SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID={os.getenv(USER_ID)};PWD={os.getenv(PASSWORD)}",'','')
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    users = []
+    sql = "SELECT * FROM user"
+    stmt = ibm_db.exec_immediate(conn, sql)
+    dictionary = ibm_db.fetch_both(stmt)
+    while dictionary != False:
+        # print ("The Name is : ",  dictionary)
+        users.append(dictionary)
+        dictionary = ibm_db.fetch_both(stmt)
+
+    if users:
+        return render_template("index.html", users = users)
+    else:
+        return render_template("signup.html")
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/adduser',methods = ['POST', 'GET'])
+def adduser():
+  if request.method == 'POST':
+
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+
+    sql = "SELECT * FROM user WHERE email =?"
+    stmt = ibm_db.prepare(conn, sql)
+    ibm_db.bind_param(stmt,1,email)
+    ibm_db.execute(stmt)
+    account = ibm_db.fetch_assoc(stmt)
+
+    if account:
+      return render_template('index.html', msg="You are already a member, please login using your details")
+    else:
+      insert_sql = "INSERT INTO user VALUES (?,?,?)"
+      prep_stmt = ibm_db.prepare(conn, insert_sql)
+      ibm_db.bind_param(prep_stmt, 1, name)
+      ibm_db.bind_param(prep_stmt, 2, email)
+      ibm_db.bind_param(prep_stmt, 3, password)
+      ibm_db.execute(prep_stmt)
+    
+    return render_template('index.html', msg="Student Data saved successfuly..")
+
